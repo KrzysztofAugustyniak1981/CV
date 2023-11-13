08ef154602cf2efaf5feb916836e9fe2351929b3
Krzysztof Augustyniak <office@pkproject.pl>
2023-11-13 07:42:08 +0100

    Add v2html+eng+pol

--- Script.js
+++ Script.js
@@ -3,6 +3,7 @@ const whatICanGive = document.getElementById('whatICanGiveContent');
 const cv = document.getElementById('cvContent');
 const myProject = document.getElementById('myProjectContent');
 const carouselOnly = document.getElementById('carouselOnly');
+const gallery = document.getElementById('gallery');
 const kontakt = document.getElementById('kontaktContent');
 
 function WhoIam() {
@@ -19,7 +20,7 @@ function WhatICanGive() {
 
 function Cv() {
     clearAll();
-    window.open("https://www.canva.com/design/DAFsQK5aa4E/51RHTyigXAwWCyqQk5PUjQ/view?utm_content=DAFsQK5aa4E&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink");
+    window.open("https://www.canva.com/design/DAE49_-W_bE/4qDgnzaoz6MgB_6xzLEVQw/view?utm_content=DAE49_-W_bE&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink");
     document.getElementById("textScript").style.fontFamily= 'Caveat';
     document.getElementById("textScript").style.fontSize= '30px';
     cv.classList.remove("d-none");
@@ -32,6 +33,11 @@ function MyProject() {
     carouselOnly.classList.remove("d-none");
 };
 
+function MyProjectGal() {
+    clearAll();
+    gallery.classList.remove("d-none")
+}
+
 function Kontakt() {
     clearAll();
     kontakt.classList.remove("d-none");
@@ -43,14 +49,16 @@ function clearAll() {
     whatICanGive.classList.add("d-none");
     cv.classList.add("d-none");
     myProject.classList.remove("active");
+    gallery.classList.add("d-none");
     carouselOnly.classList.add("d-none");
     kontakt.classList.add("d-none");
     document.getElementById("textScript").style.fontFamily= 'Bricolage Grotesque', 'sans-serif';
-    document.getElementById("textScript").style.fontSize= '1rem';
+    document.getElementById("textScript").style.fontSize= '1.3rem';
 }
 
 document.getElementById('whoIam').addEventListener('click',WhoIam);
 document.getElementById('whatICanGive').addEventListener('click',WhatICanGive);
 document.getElementById('cv').addEventListener('click',Cv);
 document.getElementById('myProject').addEventListener('click',MyProject);
+document.getElementById('myProjectGal').addEventListener('click',MyProjectGal);
 document.getElementById('kontakt').addEventListener('click',Kontakt);
